2dc4f2e914c4542ed0c37cd7425db4cc70b9a020
abdoomr2003 <abdoomer1112003@gmail.com>
2023-10-11 13:57:23 +0300

    a

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
-def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda i: number * i, my_list))
+def square_matrix_map(matrix=[]):
+    return list(map(lambda row: list(map(lambda x: x * x, row)), matrix))
+
