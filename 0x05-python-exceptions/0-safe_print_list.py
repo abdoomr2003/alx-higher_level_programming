501b4f8494b4f3d11e737dd6f57ae159ccc458a5
abdoomr2003 <abdoomer1112003@gmail.com>
2023-10-23 16:45:54 +0300

    A

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-
-    for i in range(0, x):
+    n = 0
+    for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (ValueError, TypeError):
-            pass
-    print()
-    return count
+            print('{}'.format(my_list[i]), end="")
+            n += 1
+        except IndexError:
+            break
+    print("")
+    return (n)
--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -6,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += a ** b / i
-        except ValueError:
-            result = b + a
+        except:
+            result = a + b
             break
-    return result
+    return (result)
