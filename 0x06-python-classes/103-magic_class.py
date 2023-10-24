52ec5c1e075cba1b50e345593567cef008e46a4f
abdoomr2003 <abdoomer1112003@gmail.com>
2023-10-24 22:36:55 +0300

    A

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,40 +1,16 @@
 #!/usr/bin/python3
-"""Defines a class MagicClass"""
 import math
 
 
 class MagicClass:
-    """
-    Class that defines properties of MagicClass.
-
-    Attributes:
-        radius: radius.
-    """
     def __init__(self, radius=0):
-        """Creates new instances of MagicClass.
-
-        Args:
-            radius: radius.
-
-        Raises:
-            TypeError: radius must be a number .
-        """
         self.__radius = 0
-        if not isinstance(radius, int) and not isinstance(radius, float):
-            raise TypeError("radius must be a number")
-        else:
-            self.__radius = radius
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
 
     def area(self):
-        """Returns area
-
-        Returns: area.
-        """
-        return math.pi * self.__radius ** 2
+        return self.__radius**2 * math.pi
 
     def circumference(self):
-        """Returns circumference
-
-        Returns: circumference.
-        """
-        return 2 * math.pi * self.
+        return 2 * math.pi * self.__radius
