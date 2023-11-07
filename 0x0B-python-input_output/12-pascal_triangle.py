b1452250024ac7403b6512bf120e956945e61c64
abdoomr2003 <abdoomer1112003@gmail.com>
2023-11-07 23:06:11 +0200

    A

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
+
 """
-Defines pasacal triangle that create list of lists
+File: 12-pascal_triangle.py
+Desc: This module shows the implementation of pascal's triangle.
+Author: Gizachew Bayness (Elec Crazy)
+Date Created: Aug 2 2022
 """
 
 
 def pascal_triangle(n):
     """
-    defines triangle module
+    This function returns a list of lists of integers representing
+    the Pascal’s triangle of n
     """
-    if n <= 0:
+    if (n <= 0):
         return []
-    temp = []
-    x = []
-    for i in range(n):
-        row = []
-        for j in range(i + 1):
-            if i == 0 or j == 0 or i == j:
-                row.append(1)
-            else:
-                row.append(l[j] + l[j - 1])
-        x = row
-        temp.append(row)
-    return temp
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-"""Module: 7-add_item
-This is a script that adds all arguments to a Python list.
-Save them to a file.
-"""
-
-
-import json
+# 7-add_item.py
+# Simon  Tagbor <simontagbor360@gmail.com>
+""" create a list from all arguments """
 import sys
-import os.path
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-new_file = "add_item.json"
-new_list = []
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-if os.path.exists(new_file) and os.path.getsize(new_file) > 0:
-    new_list = load_from_json_file(new_file)
-
-if len(sys.argv) > 1:
-    for elem in sys.argv[1:]:
-        new_list.append(elem)
-
-save_to_json_file(new_list, new_file)
+    try:
+        arguments = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        arguments = []
+    arguments.extend(sys.argv[1:])
+    save_to_json_file(arguments, "add_item.json")
