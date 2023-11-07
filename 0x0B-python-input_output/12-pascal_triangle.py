b53a04a21ab056eaf45f60738ab99c4fe7081a9d
abdoomr2003 <abdoomer1112003@gmail.com>
2023-11-07 22:56:48 +0200

    A

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -11,7 +11,7 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     temp = []
-    i = []
+    x = []
     for i in range(n):
         row = []
         for j in range(i + 1):
@@ -19,6 +19,6 @@ def pascal_triangle(n):
                 row.append(1)
             else:
                 row.append(l[j] + l[j - 1])
-        i = row
+        x = row
         temp.append(row)
     return temp
