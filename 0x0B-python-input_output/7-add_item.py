e8f1fec0c5682a1c7ed6012222f4ad004e80e6eb
abdoomr2003 <abdoomer1112003@gmail.com>
2023-11-07 22:58:35 +0200

    A

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""Program to save strings from command line arguments to file called
-`add_item.json`. File contains a json serialized list of all strings
-entered as arguments to the program.
+"""Module: 7-add_item
+This is a script that adds all arguments to a Python list.
+Save them to a file.
 """
 
-if __name__ == "__main__":
-    import sys
-    import json
-    save_to_json_file = \
-        __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
 
-    filename = "add_item.json"
-    with open(filename, 'a+') as f:  # Create add_item.json, if necessary
-        if f.tell() == 0:
-            json.dump([], f)
-    file_data = load_from_json_file("add_item.json")
-    if len(sys.argv) > 1:
-        file_data.extend(sys.argv[1:])
-    save_to_json_file(file_data, filename)
+import json
+import sys
+import os.path
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+new_file = "add_item.json"
+new_list = []
+
+if os.path.exists(new_file) and os.path.getsize(new_file) > 0:
+    new_list = load_from_json_file(new_file)
+
+if len(sys.argv) > 1:
+    for elem in sys.argv[1:]:
+        new_list.append(elem)
+
+save_to_json_file(new_list, new_file)
