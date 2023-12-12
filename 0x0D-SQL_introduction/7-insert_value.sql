b5f99c22a4b21e9430824e5b81f155930420a9c3
abdoomr2003 <abdoomer1112003@gmail.com>
2023-12-12 20:32:38 +0200

    A

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -5,4 +5,4 @@
 --      -- name = Best School
 -- The database name will be passed as an argument of the mysql command
 
-INSERT INTO first_table (id, name) VALUES (id = 89, name = "Best School");
+INSERT INTO first_table VALUES (89, "Best School");
