66dd27584922486144acdc67aedf0e9feca0e82e
abdoomr2003 <abdoomer1112003@gmail.com>
2023-12-12 21:42:26 +0200

    A

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -6,4 +6,5 @@
 --    -- The database name will be passed as an argument to the mysql command
 
 -- SELECT score, name FROM second_table ORDER BY score DESC;
+
 SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
