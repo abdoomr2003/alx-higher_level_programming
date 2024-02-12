4caed50437ac7c93e283a9ddf9b04fb127e10e1a
abdoomr2003 <abdoomer1112003@gmail.com>
2024-02-12 23:33:10 +0200

    A

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -11,5 +11,6 @@ let argv = process.argv.slice(2);
 for (let i = 0; argv[i] !== undefined; i++) {
   argv[i] = Number(argv[i]);
 }
-argv = Number(argv.sort().slice(-2, -1));
+console.log(argv.sort((a, b) => a - b));
+argv = Number(argv.sort((a, b) => a - b).slice(-2, -1));
 console.log(argv);
