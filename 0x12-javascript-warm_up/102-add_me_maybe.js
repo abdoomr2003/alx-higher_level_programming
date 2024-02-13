dfb09e7863b3ccd53491e17338570cc6c834f9c7
abdoomr2003 <abdoomer1112003@gmail.com>
2024-02-13 02:57:01 +0200

    A

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -4,8 +4,6 @@
  * The function must be visible from outside
  * Prototype: function (number, theFunction)
  * I'm not allowed to use var
- * @param {is a var pass from main file} number
- * @param {* function that call in main file} theFunction
  */
 exports.addMeMaybe = function (number, theFunction) {
   theFunction(++number);
