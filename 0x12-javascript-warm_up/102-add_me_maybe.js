1e14942b693e4cd535829779f1153cc59a3148a9
abdoomr2003 <abdoomer1112003@gmail.com>
2024-02-13 02:54:29 +0200

    A

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,11 +1,11 @@
-#!/us/bin/node
+#!/usr/bin/node
 /**
  * Write a function that increments and calls a function.
  * The function must be visible from outside
  * Prototype: function (number, theFunction)
  * I'm not allowed to use var
  * @param {is a var pass from main file} number
- * @param {* fanction that call in main file} theFunction
+ * @param {* function that call in main file} theFunction
  */
 exports.addMeMaybe = function (number, theFunction) {
   theFunction(++number);
