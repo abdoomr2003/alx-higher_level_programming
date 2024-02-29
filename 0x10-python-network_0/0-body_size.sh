4dd6f3051ac6f218ee5ad17cb4fc356584f5894a
abdoomr2003 <abdoomer1112003@gmail.com>
2024-02-29 21:31:20 +0200

    creating README.md 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-#request to an URL,and displays the size of the body of the response
-curl -s "$1" | wc -c
+# This  Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+
+# Check if URL argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send request to the URL and display the size of the response body
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
---
+++ 0x10-python-network_0/README.md
@@ -0,0 +1,3 @@
+# This is 0x10. Python - Network #0
+
+## First Deadline
