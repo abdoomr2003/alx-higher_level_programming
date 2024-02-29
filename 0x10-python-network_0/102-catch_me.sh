b1588d1b505c22a53eaf45e577e210aabcd6bf9f
abdoomr2003 <abdoomer1112003@gmail.com>
2024-03-01 01:38:38 +0200

    A

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
-curl -X POST -d "You got me!" 0.0.0.0:5000/catch_me
+# Make a POST request to 0.0.0.0:5000/catch_me and include a custom header
+curl -sX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin:HolbertonSchool"
