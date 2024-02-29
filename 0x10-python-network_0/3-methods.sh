057b77b701ff95e165443213d34d9933d4b17414
abdoomr2003 <abdoomer1112003@gmail.com>
2024-02-29 22:24:14 +0200

    A

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#script that takes in a URL and displays all HTTP methods the server will accept
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+# Bash script that takes in a URL and displays all HTTP methods the server will accept
+curl -sI "$1" | grep -i Allow | awk '{print $2, $3, $4}'
