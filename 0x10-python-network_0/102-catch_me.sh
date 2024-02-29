0f3b1e0ec040f9e0a606b1d3ebb0ba13dcff65a3
abdoomr2003 <abdoomer1112003@gmail.com>
2024-03-01 01:46:32 +0200

    A

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a POST request to 0.0.0.0:5000/catch_me and include a custom header
-curl -s -X GET 'http://0.0.0.0:5000/catch_me' | grep -q "You got me!" && printf "You got me!\n" || printf "Failed to catch me.\n"
+curl -s -X PUT 'http://0.0.0.0:5000/catch_me' -L -d "user_id=98" -H "Origin: HolbertonSchool" --referer "0.0.0.0:5000/catch_me" | grep -q "You got me!" && printf "You got me!\n" || printf "Failed to catch me.\n"
