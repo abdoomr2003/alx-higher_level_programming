df81ed66db147b650df195295950b2cf7ceb77e8
abdoomr2003 <abdoomer1112003@gmail.com>
2024-03-01 22:33:22 +0200

    """Write a Python script that takes in a URL and an email, sends a POST request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Write a Python script that takes in a URL and an email, sends a POST request
 to the passed URL with the email as a parameter, and displays the body of the
-response (decoded in utf-8)
-
+response (decoded in utf-8)"""
+"""
 The email must be sent in the email variable
 You must use the packages urllib and sys
 You are not allowed to import packages other than urllib and sys
