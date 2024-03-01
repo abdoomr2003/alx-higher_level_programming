b4ab042c756fb7e16273b39400374c9bed651d38
abdoomr2003 <abdoomer1112003@gmail.com>
2024-03-01 22:39:40 +0200

    """Write a Python script that fetches https://alx-intranet.hbtn.io/status"""

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches https://alx-intranet.hbtn.io/status"""
+"""
+You must use the package requests
+You are not allow to import packages other than requests
+The body of the response must be displayed like the following example (tabulation before -)
+"""
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
