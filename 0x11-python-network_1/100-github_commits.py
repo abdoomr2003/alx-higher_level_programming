bb3f1435a0c13f97a7bfeaa1485632c4fa0ab0c9
abdoomr2003 <abdoomer1112003@gmail.com>
2024-03-01 23:35:01 +0200

    """ Write a Python script that takes 2 args in order to solve this challenge.

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Write a Python script that takes 2 arguments in order to solve this challenge.
+""" Write a Python script that takes 2 args in order to solve this challenge.
 The first argument will be the repository name
 The second argument will be the owner name
 You must use the packages requests and sys
