7af2013909dfa68d85ca6b14d53413bec7ad4e1e
abdoomr2003 <abdoomer1112003@gmail.com>
2024-03-01 17:33:23 +0200

    A

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,7 +2,6 @@
 """Fetches https://intpycoranet.hbtn.io/status"""
 import urllib.request
 import sys
-
-
-with urllib.request.urlopen(sys.argv[1]) as res:
-    print(res.headers['X-Request-Id'])
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        print(res.headers['X-Request-Id'])
