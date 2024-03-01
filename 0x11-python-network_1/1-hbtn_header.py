9bd4da9cb537502566fa620ce5559e620674fe97
abdoomr2003 <abdoomer1112003@gmail.com>
2024-03-01 17:28:30 +0200

    modifay 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
 import urllib.request
 import sys
+"""Fetches https://intranet.hbtn.io/status"""
+
+
 with urllib.request.urlopen(sys.argv[1]) as res:
     print(res.headers['X-Request-Id'])
