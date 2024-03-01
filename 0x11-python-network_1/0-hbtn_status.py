50667af13620da1d3271abf58a6f23a6a74e158e
abdoomr2003 <abdoomer1112003@gmail.com>
2024-03-01 16:56:54 +0200

    A

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-from urllib import request
-
-print(dir(request))
-request.urlunparse("https://alx-intranet.hbtn.io/status")
+"""Fetches https://intranet.hbtn.io/status"""
+import urllib.request
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+   html = response.read()
+   print("Body response:")
+   print(f"\t- type: {type(html)}")
+   print(f"\t- content: {html}")
+   print(f"\t- utf8 content: {html.decode('utf-8')}")
