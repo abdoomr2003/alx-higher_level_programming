328870266bd0e267db849f5c3d2603398c1e82eb
abdoomr2003 <abdoomer1112003@gmail.com>
2024-03-01 16:59:17 +0200

    last modifaid

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,8 +2,8 @@
 """Fetches https://intranet.hbtn.io/status"""
 import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-   html = response.read()
-   print("Body response:")
-   print(f"\t- type: {type(html)}")
-   print(f"\t- content: {html}")
-   print(f"\t- utf8 content: {html.decode('utf-8')}")
+    html = response.read()
+    print("Body response:")
+    print(f"\t- type: {type(html)}")
+    print(f"\t- content: {html}")
+    print(f"\t- utf8 content: {html.decode('utf-8')}")
