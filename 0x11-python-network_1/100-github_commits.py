971a39acaf9ad6190c445306b6b5e514af62c08b
abdoomr2003 <abdoomer1112003@gmail.com>
2024-03-01 23:33:38 +0200

    """ Write a Python script that takes 2 arguments in order to solve this challenge.

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Write a Python script that takes 2 arguments in order to solve this challenge.
+The first argument will be the repository name
+The second argument will be the owner name
+You must use the packages requests and sys
+You are not allowed to import packages other than requests and sys
+You don’t need to check arguments passed to the script (number or type)
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+    response = requests.get(url)
+    commits = response.json()
+
+    for commit in commits[:10]:  # Get the first 10 commits
+        sha = commit['sha']
+        author_name = commit['commit']['author']['name']
+        print(f"{sha}: {author_name}")
