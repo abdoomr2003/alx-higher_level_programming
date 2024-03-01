e6745dd2fd22085d056c99964beefd388f9eee22
abdoomr2003 <abdoomer1112003@gmail.com>
2024-03-01 22:49:15 +0200

    """script fetch URL, send req, display X-Request-Id value in the resp header"""

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
-"""Fetches https://intpycoranet.hbtn.io/status"""
+"""script fetch URL, send req, display X-Request-Id value in the resp header"""
 import urllib.request
 import sys
+"""
+You must use the packages urllib and sys
+You are not allowed to import packages other than urllib and sys
+The value of this variable is different for each request
+You don’t need to check arguments passed to the script (number or type)
+You must use a with statement
+"""
+
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as res:
-        # print(dir(res.headers))
-        print(res.headers.__dict__)
+    url = sys.argv[1]
+
+    req = urllib.request.Request(url)
+
+    with urllib.request.urlopen(req) as response:
+        print(response.headers.get('X-Request-Id'))
