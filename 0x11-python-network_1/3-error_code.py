10ada47fe5ce5bb7df5691c0dc31079d2af976d6
abdoomr2003 <abdoomer1112003@gmail.com>
2024-03-01 23:53:27 +0200

    """Write a Python script that takes in a URL, sends a request to the URL"""

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Write a Python script that takes in a URL, sends a request to the URL, and
-displays the body of the response (decoded in utf-8).
+"""Write a Python script that takes in a URL, sends a request to the URL"""
+"""
+and displays the body of the response (decoded in utf-8).
 
 You have to manage urllib.error.HTTPError exceptions and print: Error code:
 followed by the HTTP status code
@@ -18,7 +19,7 @@ if __name__ == "__main__":
 
     try:
         with urllib.request.urlopen(url) as response:
-            body = response.read().decode('utf-8')
+            body = response.read()
             print(body)
     except urllib.error.HTTPError as e:
         print("Error code:", e.code)
