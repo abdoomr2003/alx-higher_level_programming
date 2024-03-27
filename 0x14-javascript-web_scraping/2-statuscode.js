32f4114ca9ce00b76b530c2a6e339eea9b6459f8
abdoomr2003 <abdoomer1112003@gmail.com>
2024-03-27 03:17:40 +0200

    const request = require('request');

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+
+const link = process.argv[2];
+
+request.get(link, (err, response) =>{
+        if (err){
+                console.error(err)
+        };
+                console.log('code:', response.statusCode);
+});
+
